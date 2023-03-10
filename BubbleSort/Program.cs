61dda6eb226164adfe8a6839b969b70a0a45c9f7
Pascal Betke <your@example.com>
2023-03-10 22:17:23 +0100

    Add project files.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BubbleSort;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 5, 3, 4 , 2, 1, 10, 6, 7, 9, 8 };
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            for (int j = 0; j < numbers.Length - 1; j++)
+            {
+                var tmpNumber = numbers[j + 1];
+                if (numbers[j] > numbers[j + 1])
+                {
+                    numbers[j + 1] = numbers[j];
+                    numbers[j] = tmpNumber;
+                }
+            }
+        }
+
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number.ToString());
+        }
+    }
+}
